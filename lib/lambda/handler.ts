a9d2aaf42a4e6af63a7e4828a19313f4a2971ba9
nitesh <srivastavanitesh16@gmail.com>
2023-05-22 19:18:18 +0530

    commit

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -2,7 +2,7 @@
 export async function handler(event: string, context: string) {
     console.log('Stage Name is: ' + process.env.stage);
     return {
-      body: 'Hello from a Lambda Function test cdk',
+      body: 'Hello from a Lambda Function test cdk hello',
       statusCode: 200,
     };
   }
