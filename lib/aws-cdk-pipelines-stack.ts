4a0eb1a37000fd0e2766d8eef7175c4b801fcc30
nitesh <srivastavanitesh16@gmail.com>
2023-05-22 16:44:12 +0530

    my commit

--- lib/aws-cdk-pipelines-stack.ts
+++ lib/aws-cdk-pipelines-stack.ts
@@ -2,13 +2,13 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep, Step } from 'aws-cdk-lib/pipelines';
 // import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
-// import { MyPipelineAppStage } from './stage';
+import { MyPipelineAppStage } from './stage';
 
 export class AwsCdkPipelinesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'Pipeline', {
+    const pipeline= new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.connection('nsriva13/aws-cdk-pipelines', 'main',
@@ -20,16 +20,16 @@ export class AwsCdkPipelinesStack extends cdk.Stack {
                    'npx cdk synth']
         })
     });
-  // const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
-  //     env: { account: "058453584624", region: "ca-central-1" }
-  //   }));
+  const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "058453584624", region: "ca-central-1" }
+    }));
 
 
     // testingStage.addPre(new ShellStep("Run Unit Tests", { commands: ['npm install', 'npm test'] }));
     // testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
 
     // const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
-    //   env: { account: "755314965794", region: "us-east-1" }
+    //   env: { account: "<accountno>", region: "us-east-1" }
     // }));
   }
 }
