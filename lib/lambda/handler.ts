495b9421796bdbee3915444005dfe8aea496a53d
nitesh <nitesh.srivastava@telus.com>
2023-06-06 19:15:44 +0530

    test commit

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -2,7 +2,7 @@
 export async function handler(event: string, context: string) {
     console.log('Stage Name is: ' + process.env.stage);
     return {
-      body: 'Hello from a Lambda Function test cdk hello test lambda',
+      body: 'Hello from a Lambda Function test cdk hello test lambda!!!',
       statusCode: 200,
     };
   }
